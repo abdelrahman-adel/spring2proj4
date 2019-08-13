fab927807aebb90df39a391e810482f19ec1773d
Abd-Elrahman Adel <abdelrahman-adel@hotmail.com>
2019-08-13 15:46:18 +0200

    remove spring boot

--- src/main/java/com/master/spring/spring2proj4/main/Spring2proj4Application.java
+++ src/main/java/com/master/spring/spring2proj4/main/Spring2proj4Application.java
@@ -2,21 +2,21 @@ package com.master.spring.spring2proj4.main;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import com.master.spring.spring2proj4.res.BinarySearchImpl;
 
 @ComponentScan(basePackages = { "com.master.spring.spring2proj4.res" })
-@SpringBootApplication
+@Configuration
 public class Spring2proj4Application {
 
 	private static Logger LOGGER = LoggerFactory.getLogger(Spring2proj4Application.class);
 
 	public static void main(String[] args) {
-		ApplicationContext applicationContext = SpringApplication.run(Spring2proj4Application.class, args);
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(Spring2proj4Application.class);
 
 		BinarySearchImpl binarySearchImpl = applicationContext.getBean(BinarySearchImpl.class);
 
