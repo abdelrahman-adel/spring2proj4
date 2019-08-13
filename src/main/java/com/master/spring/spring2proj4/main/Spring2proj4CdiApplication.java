48c82c005b5122694ee3c2d6fd1fdc3d02f52446
Abd-Elrahman Adel <abdelrahman-adel@hotmail.com>
2019-08-13 16:55:10 +0200

    lazy

--- src/main/java/com/master/spring/spring2proj4/main/Spring2proj4CdiApplication.java
+++ src/main/java/com/master/spring/spring2proj4/main/Spring2proj4CdiApplication.java
@@ -2,7 +2,6 @@ package com.master.spring.spring2proj4.main;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -16,12 +15,13 @@ public class Spring2proj4CdiApplication {
 	private static Logger LOGGER = LoggerFactory.getLogger(Spring2proj4CdiApplication.class);
 
 	public static void main(String[] args) {
-		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(
-				Spring2proj4CdiApplication.class);
+		try (AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(
+				Spring2proj4CdiApplication.class)) {
 
-		SomeCdiBusiness someCdiBusiness = applicationContext.getBean(SomeCdiBusiness.class);
+			SomeCdiBusiness someCdiBusiness = applicationContext.getBean(SomeCdiBusiness.class);
 
-		LOGGER.info("{} - {}", someCdiBusiness, someCdiBusiness.getSomeCdiDao());
+			LOGGER.info("{} - {}", someCdiBusiness, someCdiBusiness.getSomeCdiDao());
+		}
 	}
 
 }
