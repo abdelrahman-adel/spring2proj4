4ed24d8482c82078f20fa831baea3b729489d458
Abd-Elrahman Adel <abdelrahman-adel@hotmail.com>
2019-09-03 11:00:08 +0200

    Update Spring2proj4CdiApplication.java

--- src/main/java/com/master/spring/spring2proj4/main/Spring2proj4CdiApplication.java
+++ src/main/java/com/master/spring/spring2proj4/main/Spring2proj4CdiApplication.java
@@ -1,18 +1,13 @@
 package com.master.spring.spring2proj4.main;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import com.master.spring.spring2proj4.res.cdi.SomeCdiBusiness;
 
 @ComponentScan(basePackages = { "com.master.spring.spring2proj4.res" })
 @Configuration
 public class Spring2proj4CdiApplication {
 
-	private static Logger LOGGER = LoggerFactory.getLogger(Spring2proj4CdiApplication.class);
+//	private static Logger LOGGER = LoggerFactory.getLogger(Spring2proj4CdiApplication.class);
 
 	/**
 	 * Commented for maven build (two main methods in one application)
